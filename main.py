94793253722f7636194bfff7872e7ef700be282f
root <root@DESKTOP-JO7GQ53.localdomain>
2026-04-23 16:18:29 +0800

    update

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     engine = CardiacEchoEngine()
     engine.list_modules()  # 自动打印所有可用功能
 
+    #### 左心室 分割
     # engine.run("lv_segmentation", save_video=True)
 
+    #### 左心室 射血分数预测
     engine.run("lv_ef_prediction")
